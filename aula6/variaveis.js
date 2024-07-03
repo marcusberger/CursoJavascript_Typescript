75c8911762b053670e077ef36c333c36935d0437
Marcus Berger <marcusvcberger@gmail.com>
2024-07-03 13:48:46 -0300

    aula 6

---
+++ aula6/variaveis.js
@@ -0,0 +1,21 @@
+let nome = 'João';
+
+console.log(nome, ' nasceu em 1984.');
+console.log('Em 2000', nome , 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome , 'em 2015.');
+console.log('O filho de', nome ,'se chama Eduardo.');
+
+
+let name; // declarou a variável
+name = 'Marcus'; //incializando a variável
+console.log(name);
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não pode conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não utilize var, utilize let.
