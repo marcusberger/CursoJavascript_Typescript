b9b9f8e8d9ec15954da66fa91b5b4ddbadc599fb
Marcus Berger <marcusvcberger@gmail.com>
2024-07-04 00:04:42 -0300

    aula 10

---
+++ aula10/operadoresAritmeticosAtribuicaoIncremento.js
@@ -0,0 +1,19 @@
+/**
+ * Aritméticos
+ * + Adição / Concatenação
+ * - / *
+ *  ** Potenciação
+ *  % resto da divisão
+ * 
+ * ordem: ()
+ * **
+ * * / %
+ * + -
+ * Incremento = ++
+ * Decremento = --
+ * 
+*/
+
+const num1 = 2;
+const num2 = 10;
+console.log(num1 % num2)
---
+++ aula9/tiposDadosPrimitivos.js
@@ -0,0 +1,8 @@
+const nome = 'Marcus' //string
+const num = 10; //number
+const num1 = 1.52; //number
+let nomeAluno; //undefined -> não aponta para local nenhum na memoria
+const sobrenome = null; //nulo -> não aponta para local nenhum na memoria
+const aprovado = true; //boolean = true ou false
+
+
