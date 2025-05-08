6bbe07e8f488692eebfae683b6f2f6b5bccd198f
MarcusBerger <marcusvcberger@gmail.com>
2025-05-08 00:22:25 -0300

    dia 2 estudando javascript

--- LógicaProgramaçãoJS/OperadoresComparacao/ifElseIfeElse.js
+++ LógicaProgramaçãoJS/OperadoresComparacao/ifElseIfeElse.js
@@ -4,8 +4,14 @@ Entre 12-17 = boa tarde
 Entre 18-23 = Boa noite
 */
 
-const hora = 13;
+const hora = 18;
 
 if (hora >= 0 && hora <= 11){
     console.log('Bom dia!');
+} else if (hora >= 12 && hora <= 17){
+    console.log('Boa tarde!');
+} else if (hora => 18 && hora <= 23) {
+    console.log('Boa noite!');
+} else {
+    console.log('Olá!');
 }
